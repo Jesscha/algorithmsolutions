a433dbd5fccc470bb4d512340e7905865387bbf8
jessecha <jessecha222@gmail.com>
2020-04-20 21:26:48 +0900

    refactored

--- kakao2018zip.js
+++ kakao2018zip.js
@@ -1,43 +1,27 @@
 
 
 function solution(msg) {
-    const alphas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    const alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+    console.log(alphas)
     const ans = [] 
-    const visit = []
-    for(let i = 0; i < msg.length; i++){
-        visit.push(0)
-    }
-    let txtlen = 1
-    let textbefore
-    for(let i = 0; i< msg.length; i++){
-        if (txtlen !== 1 ){
-            txtlen -=1
-            continue
+    
+    while(msg) {
+        let temp = '';
+        for ( i = 0; i< msg.length; i ++) {
+            temp = msg.slice(0, i+1);
+            if (alphas.indexOf(temp)=== -1){
+                ans.push(alphas.indexOf(temp.slice(0,temp.length-1))+1);
+                break
+            }
+            if (i === msg.length -1 ){
+                ans.push(alphas.indexOf(temp.slice(0,temp.length))+1);
+            }
         }
-        let text = msg[i]
-        let ii = i 
-        while (alphas.includes(text)){
-            ii += 1 
-            textbefore = text
-            text += msg[ii]
-        }
-        ans.push(alphas.indexOf(textbefore)+1)
-        alphas.push(text)
-        txtlen = textbefore.length
+        alphas.push(temp);
+        console.log(alphas)
+        msg = msg.slice(i)
     }
     return ans;
 }
 
 console.log(solution("KAKAO"))
-
-/*
-풀이 방법 
-
-인덱스 배열과 값 배열을 만든다.
-
-언제까지 글자수를 체크해야 할지 모르겠다. 
-비짓을 만들면 해결 할 수 있다. 해당 글자가 비짓 되었으면 패스를 하자 
-
-
-*/
-
