4a8511ab4f0e23f24247ba766f166271a4a10737
jessecha <jessecha222@gmail.com>
2020-04-22 12:33:40 +0900

    edit line

--- numberofline.py
+++ numberofline.py
@@ -5,8 +5,7 @@ def solution(n, k):
     while n > 0:
         n -= 1
         p, r= divmod(k, m.factorial(n))
-        if r == 0:
-            p-= 1
+        if r == 0: p-= 1
         answerList.append(numList[p])
         numList.remove(numList[p])
         k = r 
