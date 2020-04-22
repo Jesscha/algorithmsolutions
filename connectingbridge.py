e6d5997eb8f3c75fe2942678d75bb7094582ab9b
jessecha <jessecha222@gmail.com>
2020-04-22 18:23:29 +0900

    passed

--- connectingbridge.py
+++ connectingbridge.py
@@ -1,54 +1,52 @@
-
+import heapq as hq
 
 def solution(n, costs):
-    answer = []
-    graph = [[0 for _ in range(n)] for _ in range(n)] 
-    visit = [0 for _ in range(n)]
+    answer = 0
+    costList = [[] for _ in range(n)]
 
+    for f, t, c in costs:
+        costList[f].append((t,c))
+        costList[t].append((f,c))
+    workList = [] 
+    visit = [0]*n
 
-    for cost in costs:
-        graph[cost[0]][cost[1]] = cost[2]
-        graph[cost[1]][cost[0]] = cost[2]
-    print(graph)
+    hq.heappush(workList, (0,0))
+    while 0 in visit:
+        c, p = hq.heappop(workList)
+        if visit[p]:
+            continue
+        answer += c
+        visit[p] = 1
 
-    def dfs(p, c, v):
-        if v == []:
-            v = []
-        if len(v) == n :
-            print(v)
-            answer.append(c)
-        for i in range(len(graph[p])):
-            # print(graph[p][i])
-            # print(v[i])
-            if graph[p][i] != 0 and i not in v:
-                c += graph[p][i]
-                v.append(i)
-                dfs(i, c, v)
-    
-    dfs(0, 0, [0])
-    
-    
+        for t, c in costList[p]:
+            if visit[t] == 0:
+                hq.heappush(workList, (c, t))
+            
     return answer
 print(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]))
 
 
 '''
 
-브루트 포스 로 풀수 있지 않나?
+주어진 배열을 정리한다
 
-전체 그래프 관계를 그리고 거기에 맞춰서 탐색을 하면 되지 않나 
+위의 방법대로 하면, 힙안에 코스트는 계속 남아있고 비짓을 한 경우 그건 무시를 하게 되기 때문에 워킹하는 코드가 된다. 
 
-dfs로 만들어야 겠네 
+구현 방법
 
-과재 1 주어진 costs를 그래프로 만들기 
+일단 빈 리스트를 만든다.
 
-[i][j] 에 코스트가 적혀있다. 
+주어진 코스트를 바탕으로 각 리스트에 원소를 추가한다 원소는 (갈수있는 곳, 가격)이다. 
 
-과제 2 dfs를 돌리기 
+heapq용 리스트 만들고 거기에 (0,0)을 푸시한다. 
 
-visit과 cost, position을 인자로 받음 
+새롭게 들어온 것의 첫번째 원소를 visit 처리 한다. 
 
-visit에 더 방문할 곳이 없으면 종료,cost 반환 
+리스트 상에 해당 원소 for 문을 돌린다. 방문 하지 않은 것들을 heapq에 담는다. 코스트가 먼저 들어가게 해야 heapq정렬이 빠르게 잘 됨 
+
+만약 visit이 모두 되었으면 종료한다. 
+
+
 
 
 '''
