edd38ce4d2f754840ea579ed71762b23c8722d14
jessecha <jessecha222@gmail.com>
2020-04-29 13:34:20 +0900

    passed

--- completion.js
+++ completion.js
@@ -1,14 +1,18 @@
 function solution(participant, completion) {
-    let answer = []
-    for (let i = 0; i < participant.length; i++) {
-        if ( completion.includes(participant[i]) === false){
-            return participant
-        }else{
-            const idx = completion.indexOf(participant[i])
-            completion.splice(idx, 1)
-        }
-    }
-    return answer[0];
+   let ret = [] 
+   let hased = [] 
+   participant.forEach(entry => {
+       hased[entry] = hased[entry] ? hased[entry] + 1 : 1
+   })
+   completion.forEach(entry => {
+       hased[entry] = hased[entry] -1 
+   })
+
+   for (let key in hased) {
+       if (hased[key] >= 1 ) return key
+   }
+
+
 }
 
 console.log(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
